8da8208c7e58d4e8c2cd7e41e56d88ff1b97cad3
Javantax <79591148+Javantax@users.noreply.github.com>
2024-08-13 04:10:46 -0400

    Made a 16x16 grid of boxes

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+}
+
+const div = document.querySelector("div");
+
+
+
