8a19e60db68577fb43ef4fa00269d288ec1cdfc0
Javantax <79591148+Javantax@users.noreply.github.com>
2024-08-14 04:55:59 -0400

    Add hover effect on boxes & grid-size button

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector(".container");
+let gridSize = 16; // default grid size
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.classList.add("box");
-    container.appendChild(div);
+function gridCreator(gridSize) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        let div = document.createElement("div");
+        div.classList.add("box");
+        container.appendChild(div);
+    }
+
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("colour-change");
+        })
+    });
 }
+gridCreator(gridSize);
 
-const div = document.querySelector("div");
+const gridSizeButton = document.querySelector(".grid-size");
+gridSizeButton.addEventListener("click", () => {
+    let newGridSize = parseInt(prompt("Enter a grid size? (for one side)", "16"));
+    if (newGridSize > 100) {
+        alert("Number should be smaller than 100");
+    }
+    if (newGridSize < 100) {
+        container.innerHTML = ""; // clear out existing grid
+        gridCreator(newGridSize);
+    }
+});
 
 
 
