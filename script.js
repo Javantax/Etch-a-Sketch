4693dfbc75ba63ffa6d6aea97b64f39f55e15f6a
Javantax <79591148+Javantax@users.noreply.github.com>
2024-08-16 03:42:15 -0400

    Add rgb button to color the box with random colors

--- script.js
+++ script.js
@@ -16,10 +16,21 @@ function gridCreator(gridSize) {
             box.classList.add("colour-change");
         })
     });
+
+    const rgbButton = document.querySelector(".rgb-colors");
+
+    rgbButton.addEventListener("click", () => boxes.forEach(box => {
+        box.style.width = `${760/gridSize}px`;
+        box.style.height = `${680/gridSize}px`;
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = `rgb(${rgbColorGen()}, ${rgbColorGen()}, ${rgbColorGen()})`;
+        })
+    }))
+
 }
 gridCreator(gridSize);
 
-const gridSizeButton = document.querySelector(".grid-size");
+const gridSizeButton = document.querySelector(".grid-size-button");
 gridSizeButton.addEventListener("click", () => {
     let newGridSize = parseInt(prompt("Enter a grid size? (for one side)", "16"));
     if (newGridSize > 100) {
@@ -30,6 +41,11 @@ gridSizeButton.addEventListener("click", () => {
         gridCreator(newGridSize);
     }
 });
+
+
+function rgbColorGen() {
+    return Math.floor(Math.random() * 255);
+}
 
 
 
